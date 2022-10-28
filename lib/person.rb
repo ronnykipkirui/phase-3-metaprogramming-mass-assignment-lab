0823b8bac9c3a68c5d48702fdedd8c3ec2f1006b
Ronny <ronnybet41@gmail.com>
2022-10-28 16:31:40 +0300

    completed assignment

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,11 @@
 class Person
   # your code here
+
+  def initialize(arguments)
+    arguments.each do |key,value|
+      self.class.attr_accessor(key)
+      self.send("#{key}=",value)
+    end
+
+  end
 end
